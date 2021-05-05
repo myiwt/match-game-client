6d6ac2f56b7bbcd502709ecb7613cdba876f0433
Megan Teh <ywtm@protonmail.com>
2021-05-05 12:30:38 +1200

    fix: fix leaderboard

--- src/js/object-oriented/Leaderboard.js
+++ src/js/object-oriented/Leaderboard.js
@@ -8,13 +8,19 @@ function getScores() {
             var scores = res.data.result;
             // Sort scores to return the top 10
             var scoresArr = [];
-            for (var score in scores) {
-                scoresArr.push([score, scores[score]]);
+            console.log(scores);
+            for (let i=0;i<scores.length;i++) {
+                scoresArr.push(scores[i]["score"], scores[i]);
             }
+        /*     for (var score in scores) {
+                scoresArr.push([score, scores[score]]);
+                console.log(score);
+            } */
 
             scoresArr.sort(function(a, b) {
                 return a[1]-b[1];
             });
+            console.log(scoresArr);
             var top5scores = [];
             // If there are fewer than 5 scores, just use all the scores for the leaderboard
             var nTopScores = scoresArr.length>=5 ? 5: scoresArr.length
@@ -22,7 +28,7 @@ function getScores() {
                 top5scores.push([scoresArr[i][1]["name"],scoresArr[i][1]["score"]]);
             }
             console.log(top5scores);
-            var leaderboardTable = "<table><tr><th>Name</th><th><Score></th></tr>";
+            var leaderboardTable = "<table><tr><th>Name</th><th>Score</th></tr>";
             for (let i=0;i<top5scores.length; i++) {
                 leaderboardTable += "<tr><td>" + top5scores[i][0] + "</td>";
                 leaderboardTable += "<td>" + top5scores[i][1] + "</td></tr>";
